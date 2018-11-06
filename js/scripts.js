9d7090f2c586f24d03921ccd925c1e9563057d3c
Oleg Shelest <lateexairu@gmail.com>
2018-11-06 23:01:13 +0200

    initial upload

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+
+  $(".owl-brands").owlCarousel({
+    items: 9,
+    autoplay: true,
+    loop: true,
+    dots: false,
+  });
+
+  $(".owl-2").owlCarousel({
+    items: 1,
+  });
+
+  $(".owl-3").owlCarousel({
+    items: 4,
+    margin: 10,
+    loop: true,
+    dots: false,
+    nav: true,
+    navText: ["", ""],
+  });
+
+  $(".owl-4").owlCarousel({
+    items: 1,
+    autoplay: true,
+    loop: true,
+    nav: true,
+    navText: ["", ""],
+    dotsContainer: ".owl-header .owl-dots",
+  });
+
+  $(".owl-5").owlCarousel({
+    items: 3,
+    margin: 80,
+    loop: true,
+    dots: false,
+    nav: true,
+    navText: ["", ""],
+  });
+
+  $('#aside-nav a').mPageScroll2id({
+    scrollSpeed: 900,
+    offset: 53,
+  });
+
+});
