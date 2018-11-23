67096da01c90f1420ade0ab346df1ea36fce13be
Yuriy <frontexport1987@gmail.com>
2018-11-22 23:20:10 -0500

    move product reviews page to a modal.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+	$("#article-product .modal-content").load("article_product.html");
+
 	const numberWithCommas = (x) => {
 		return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 	}
